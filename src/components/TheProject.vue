823ce7c6817e2790caf21093ed55cb2e0b0aa593
El Hadi EL GHOLEM <egholem@gmail.com>
2024-08-31 05:21:48 +0200

    portfolio built

--- src/components/TheProject.vue
+++ src/components/TheProject.vue
@@ -6,7 +6,7 @@
     <div class="services-section__container">
       <div class="services-section__caption">
         <figure>
-            <img :src="require('../assets/Capture d\'écran 2024-08-30 135808.png')" alt="Screenshot">
+          <img src="../assets/Capture d'écran 2024-08-30 135808.png" alt="" />
         </figure>
       </div>
       <div class="services">
